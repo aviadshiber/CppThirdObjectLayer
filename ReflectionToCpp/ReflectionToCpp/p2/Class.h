d7091a7ccbfa9b295bd71ace3ab928b487b57efb
aviadshiber <aviadshiber@campus.technion.ac.il>
2017-06-27 09:58:14 +0300

    creating a bootstrap of project

---
+++ ReflectionToCpp/ReflectionToCpp/p2/Class.h
@@ -0,0 +1,46 @@
+#ifndef CLASS_H_
+#define CLASS_H_
+#include <string>
+#include "Object.h"
+
+
+class Class{
+
+private:
+public:
+	Class(Class* c, const std::string& name);
+		
+	Class* getSuperClass();
+	
+	Object* newInstance();
+
+	void addMethod(std::string name, Func func);
+	
+	void addInstanceField(std::string name, Type t);
+
+	void addStaticField(std::string name, Type t);
+
+	Field getField(std::string name);
+
+	std::list<Field> getFields();
+
+	Method getMethod(std::string name);
+
+	std::list<Method> getMethods();
+
+	int getInt(std::string name);
+
+	void setInt(std::string name, int value);
+
+	Object* getObj(std::string name);
+
+	void setObj(std::string name, Object* value);
+	
+	std::string name() const;
+
+	static void setAccessible(bool flag);
+
+
+};
+
+#endif /* CLASS_H_ */
---
+++ ReflectionToCpp/ReflectionToCpp/p2/Field.h
@@ -0,0 +1,32 @@
+
+#ifndef FIELD_H_
+#define FIELD_H_
+#include "Exceptions.h"
+#include <string>
+
+class Object;
+
+class Field  {
+private:
+
+public:
+	Field(std::string name, std::string className, Type t, bool isStatic);
+
+	Type getType();
+	
+	void setInt(Object* obj, int value);
+
+	int getInt(Object* obj);
+	
+	void setObj(Object* obj, Object* value);
+
+	Object* getObj(Object* obj);
+
+	std::string getDeclaringClass() const;
+	
+	std::string name() const;
+
+	
+};
+
+#endif /* FIELD_H_ */
